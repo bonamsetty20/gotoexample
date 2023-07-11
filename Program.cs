8b6764026ff5ba6bf0894393b084e53a4128fb15
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-07-11 11:58:53 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gotoexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            Again:
+            Console.WriteLine("Enter user Name");
+            userName = Console.ReadLine();
+            if(userName.Length>=6)
+            {
+                Console.WriteLine("Welcome : \t" + userName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid user Name");
+                goto Again;
+            }
+            Console.ReadKey();
+        }
+    }
+}
